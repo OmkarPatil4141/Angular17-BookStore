84bdd2c6c8ac532e97758fd3c9c2ce6233f0cccc
OmkarPatil4141 <patilomkarr4141@gmail.com>
2024-05-08 17:33:02 +0530

    Header component commit

--- Book-Store/src/app/Components/header/header.component.ts
+++ Book-Store/src/app/Components/header/header.component.ts
@@ -4,8 +4,46 @@ import { Component } from '@angular/core';
   selector: 'app-header',
   standalone: true,
   imports: [],
-  templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  template: `
+    <nav class="navbar navbar-light bg-custom rounded-bottom">
+    <a class="navbar-brand" href="#">
+        <div class="logo">
+            <img src="https://www.svgrepo.com/show/125863/books.svg" width="60" height="60" class="d-inline-block align-top" alt="">
+            <div class="logo-text">Book-Store</div>
+            <div class="welcome"></div>
+        </div>
+    </a>
+</nav>
+
+  `,
+  styles: `
+
+.bg-custom {
+    background-color: none;
+}
+
+.navbar {
+    border-radius: 10px 10px 10px 10px;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.welcome {
+    font-size: 24px; 
+    margin-left: 10px;
+    color: #ff0000; 
+    font-weight: bold;
+}
+
+.logo-text {
+    font-weight: bold;
+}
+
+
+  `
 })
 export class HeaderComponent {
 
