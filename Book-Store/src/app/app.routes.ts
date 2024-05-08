44753b5e7dfcde0c039ad5beb882d7f56a400d13
OmkarPatil4141 <patilomkarr4141@gmail.com>
2024-05-08 17:17:39 +0530

    components creation and routes configuration

--- Book-Store/src/app/app.routes.ts
+++ Book-Store/src/app/app.routes.ts
@@ -1,3 +1,32 @@
 import { Routes } from '@angular/router';
+import { BookDetailsComponent } from './Components/book-details/book-details.component';
+import { BooksListComponent } from './Components/books-list/books-list.component';
+import { HomeComponent } from './Components/home/home.component';
+import { ErrorPageComponent } from './Components/error-page/error-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    
+    {
+        path :'',
+        component:HomeComponent
+
+    },
+
+    {
+        path:'books', 
+        // redirectTo:'books',
+        component:BooksListComponent,
+        
+
+    },
+    {
+        path:'books/:id',
+        component:BookDetailsComponent
+    },
+    {
+        path:'**',
+        component:ErrorPageComponent
+    },
+    
+];
