2234738afc71ec8cbe64fb9f04df9e10a0b2086f
OmkarPatil4141 <patilomkarr4141@gmail.com>
2024-05-08 19:54:28 +0530

    Completion of book-details component

--- Book-Store/src/app/Components/book-details/book-details.component.ts
+++ Book-Store/src/app/Components/book-details/book-details.component.ts
@@ -1,12 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { BookService } from '../../Services/book-service.service';
+import { error } from 'console';
+import { HeaderComponent } from '../header/header.component';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent,RouterLink],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.css'
 })
-export class BookDetailsComponent {
+export class BookDetailsComponent implements OnInit
+{
+
+  BookInfo : any;
+  errMsg : any;
+
+  constructor(private active_route:ActivatedRoute, private bookService : BookService)
+  {
+
+  }
+
+  ngOnInit(): void 
+  {
+    this.active_route.paramMap.subscribe(
+    (parameter)=>{
+        let id = Number(parameter.get('id'));
+        
+        this.bookService.getBookById(id).subscribe(
+          (BookInfo)=>{
+            console.log(BookInfo);
+            this.BookInfo = BookInfo;
+          })
+      },
+      
+    (error)=>{
+        console.log(error)
+        this.errMsg = error;
+      })   
+  }
 
 }
+
+
--- Book-Store/src/app/app.component.ts
+++ Book-Store/src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { BooksListComponent } from './Components/books-list/books-list.component';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,BooksListComponent,RouterModule,RouterLink,RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
